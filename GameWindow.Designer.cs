0c2543779677539ad76511ee6b822ed76f2a8b37
Wagner Scholl Lemos <josephblt@gmail.com>
2025-02-25 16:04:06 -0300

    Project files upload.
    Uploading all project files.

---
+++ GameWindow.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Snake
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // GameWindow
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = System.Drawing.Color.Black;
+            CausesValidation = false;
+            ClientSize = new System.Drawing.Size(800, 600);
+            DoubleBuffered = true;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "GameWindow";
+            Text = "Scholl Snake";
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+    }
+}
+
---
+++ GameWindow.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+using Snake.GameStates;
+
+namespace Snake
+{
+    public partial class GameWindow : Form
+    {
+        #region Constructor
+
+        private GameWindow()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            GameStatesManager = new GameStatesManager();
+        }
+
+        #endregion
+
+        #region Singleton
+
+        private static GameWindow instance;
+
+        public static GameWindow GetGameWindow()
+        {
+            if (instance == null)
+                instance = new GameWindow();
+            return instance;
+        }
+
+        #endregion
+
+        #region Attributes and Properties
+
+        public bool GameRunning { get; set; }
+
+        public GameStatesManager GameStatesManager { get; private set; }
+
+        public string GameWinner { get; set; }
+
+        public bool Paused { get; private set; }
+
+        #endregion        
+
+        #region Overriden Methods
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            GameRunning = false;
+            base.OnFormClosing(e);
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            GameStatesManager.InitializeManager();
+            StartMainLoop();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            if (GameStatesManager.CurrentGameState != null)
+                GameStatesManager.CurrentGameState.Render(e.Graphics);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void StartMainLoop()
+        {
+            GameRunning = true;
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var lastTime = (float)timer.Elapsed.TotalSeconds;
+
+            while (GameRunning)
+            {
+                var gameTime = (float)timer.Elapsed.TotalSeconds;
+                var elapsedTime = gameTime - lastTime;
+                lastTime = gameTime;
+                Application.DoEvents();
+
+                if (!Paused)
+                {
+                    if (!timer.IsRunning)
+                        timer.Start();
+
+                    if (GameStatesManager.CurrentGameState != null)
+                        GameStatesManager.CurrentGameState.Update(elapsedTime);
+                }
+                else
+                    timer.Stop();
+
+                Refresh();
+            }
+
+            timer.Stop();
+            GameStatesManager.FinalizeManager();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void TogglePause()
+        {
+            Paused = !Paused;
+        }
+
+        #endregion
+    }
+}
